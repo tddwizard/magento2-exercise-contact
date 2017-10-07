9ed87c29b4dfcf1d35c7760f196079320017a737
Fabian Schmengler <fs@integer-net.de>
2017-10-07 15:14:00 +0200

    Implement generated search results interface

--- Model/ResourceModel/Inquiry/Collection.php
+++ Model/ResourceModel/Inquiry/Collection.php
@@ -3,7 +3,10 @@
 
 namespace TddWizard\ExerciseContact\Model\ResourceModel\Inquiry;
 
-class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use TddWizard\ExerciseContact\Api\Data\InquirySearchResultsInterface;
+
+class Collection extends AbstractCollection implements InquirySearchResultsInterface
 {
 
     /**
@@ -17,5 +20,67 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
             'TddWizard\ExerciseContact\Model\Inquiry',
             'TddWizard\ExerciseContact\Model\ResourceModel\Inquiry'
         );
+    }
+
+    /**
+     * Get search criteria.
+     *
+     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
+     */
+    public function getSearchCriteria()
+    {
+        return $this->searchCriteria;
+    }
+
+    /**
+     * Set search criteria.
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
+    {
+        $this->searchCriteria = $searchCriteria;
+        return $this;
+    }
+
+    /**
+     * Get total count.
+     *
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+
+    /**
+     * Set total count.
+     *
+     * @param int $totalCount
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+
+    /**
+     * Set items list.
+     *
+     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
+     * @return $this
+     */
+    public function setItems(array $items = null)
+    {
+        if (!$items) {
+            return $this;
+        }
+        foreach ($items as $item) {
+            $this->addItem($item);
+        }
+        return $this;
     }
 }
