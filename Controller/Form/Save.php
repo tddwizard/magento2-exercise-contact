d57b50ff367ba7933722c565ecb639836fe03674
Fabian Schmengler <fs@integer-net.de>
2017-10-08 23:21:38 +0200

    Add input validation

--- Controller/Form/Save.php
+++ Controller/Form/Save.php
@@ -5,14 +5,36 @@ namespace TddWizard\ExerciseContact\Controller\Form;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\Result\Redirect;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\ValidatorException;
 
 class Save extends Action
 {
     public function execute()
     {
+        try {
+            $this->validateRequest();
+            $this->messageManager->addSuccessMessage('We received your inquiry and will contact you shortly.');
+        } catch (ValidatorException $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        }
+        return $this->redirectToForm();
+    }
+
+    private function redirectToForm(): Redirect
+    {
         /** @var Redirect $result */
         $result = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $result->setPath('exercise_contact/form');
         return $result;
+    }
+
+    private function validateRequest()
+    {
+        if (strpos($this->getRequest()->getParam('email'), '@') === false) {
+            throw new ValidatorException(__('Please enter a valid email address.'));
+        }
+        if (trim($this->getRequest()->getParam('message')) === '') {
+            throw new ValidatorException(__('Please enter a message.'));
+        }
     }
 }
--- Test/Integration/Controller/SaveTest.php
+++ Test/Integration/Controller/SaveTest.php
@@ -2,6 +2,7 @@
 
 namespace TddWizard\ExerciseContact\Test\Integration\Controller\Frontend;
 
+use Magento\Framework\Message\MessageInterface;
 use Magento\TestFramework\TestCase\AbstractController;
 
 class SaveTest extends AbstractController
@@ -9,9 +10,54 @@ class SaveTest extends AbstractController
     /**
      * @magentoAppArea frontend
      */
-    public function testRedirect()
+    public function testRedirectWithSuccessMessage()
     {
+        $this->getRequest()->setMethod('POST');
+        $this->getRequest()->setPostValue('email', 'test@example.com');
+        $this->getRequest()->setPostValue('message', 'Hello, World!');
         $this->dispatch('exercise_contact/form/save');
         $this->assertRedirect($this->stringContains('exercise_contact/form'));
+        $this->assertSessionMessages($this->isEmpty(), MessageInterface::TYPE_ERROR);
+        $this->assertSessionMessages(
+            $this->equalTo(['We received your inquiry and will contact you shortly.']),
+            MessageInterface::TYPE_SUCCESS
+        );
+    }
+    public static function dataInvalidInput()
+    {
+        return [
+            'empty email' => [
+                '',
+                'Hello, World!',
+                'Please enter a valid email address.'
+            ],
+            'invalid email' => [
+                'test',
+                'Hello, World!',
+                'Please enter a valid email address.'
+            ],
+            'empty message' => [
+                'test@example.com',
+                ' ',
+                'Please enter a message.'
+            ],
+        ];
+    }
+    /**
+     * @dataProvider dataInvalidInput
+     * @magentoAppArea frontend
+     */
+    public function testRedirectWithErrorMessageOnInvalidInput($email, $message, $expectedErrorMessage)
+    {
+        $this->getRequest()->setMethod('POST');
+        $this->getRequest()->setPostValue('email', $email);
+        $this->getRequest()->setPostValue('message', $message);
+        $this->dispatch('exercise_contact/form/save');
+        $this->assertRedirect($this->stringContains('exercise_contact/form'));
+        $this->assertSessionMessages($this->isEmpty(), MessageInterface::TYPE_SUCCESS);
+        $this->assertSessionMessages(
+            $this->equalTo([$expectedErrorMessage]),
+            MessageInterface::TYPE_ERROR
+        );
     }
 }
